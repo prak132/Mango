e39ca9b9758cf4bd64142dc14639a3aaa103f936
Prakhar <100105525+prak132@users.noreply.github.com>
2024-04-16 22:01:22 -0700

    fixed file structure

--- src/slider.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var rating = document.getElementById('rating');
-    rating.onchange = function() {
-        document.getElementById('value').innerHTML = this.value;
-    }
-    document.getElementById('value').innerHTML = rating.value;
-});
